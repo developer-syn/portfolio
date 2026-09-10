e6a4407619479aea3468905a695fd9853ceb37f7
developer-syn <synkyle.cael2@gmail.com>
2026-09-10 11:34:25 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/messages', function () {
+    return view('messages');
+});
+
+
 Route::get('/messages', [MessageController::class, 'create'])->name('messages.create');
 Route::post('/messages', [MessageController::class, 'store'])->name('messages.store');
